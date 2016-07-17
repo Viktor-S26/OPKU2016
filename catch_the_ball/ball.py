9734d7bc7c1c2889bfd669d599532a8451b63e47
dns <Viktor-s26@yandex.ru>
2016-07-17 22:13:18 +0500

    Эксперементы с виджетами Tkinter

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -4,9 +4,7 @@ def button1_command():
     print('Button 1 default command')
 
 def print_hello(event):
-    #print(dir(event))
-    #print(event.char)
-    #print(event.keycode)
+
     print(event.num)
     print(event.x, event.y)
     #print(event.x_root, event.y_root)
@@ -19,18 +17,37 @@ def print_hello(event):
     else:
         raise ValueError()
 
+def init_main_window():
+    """
+    Инициализация главного окна. Создание всех необходимых виджетов
+    и их упаковка.
+    :return:
+    """
+    global root, button1, button2, label, text, scale
+    root = tkinter.Tk()
+
+    button1 = tkinter.Button(root, text="Button 1", command=button1_command)
+    button1.bind("<Button>", print_hello)
+    button1.pack()
+
+    button2 = tkinter.Button(root, text="Button 2")
+    button2.bind("<Button>", print_hello)
+    button2.pack()
+
+    variable = tkinter.IntVar(0)
+    label = tkinter.Label(root, textvariable=variable)
+    scale = tkinter.Scale(root, orient=tkinter.HORIZONTAL, length=300, from_=0,
+                          to=100, tickinterval=10, resolution=5,
+                          variable=variable)
+    text = tkinter.Entry(root, textvariable=variable)
+
+    for obj in button1, button2, label, scale, text:
+        obj.pack()
 
 
-root = tkinter.Tk()
+    root.mainloop()
 
-button1 = tkinter.Button(root, text="Button 1", command=button1_command)
-button1.bind("<Button>", print_hello)
-button1.pack()
-
-button2 = tkinter.Button(root, text="Button 2")
-button2.bind("<Button>", print_hello)
-button2.pack()
-
-root.mainloop()
+if __name__ == "__main__":
+    init_main_window()
 
 # from tkinter import *
