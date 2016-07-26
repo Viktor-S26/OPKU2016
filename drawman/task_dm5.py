631c3a8dadb79ca17a14e4cd80d4565e2d73902d
dns <Viktor-s26@yandex.ru>
2016-07-26 14:34:23 +0500

    Тренировочные задачи для Чертежника созданные самостоятельно

--- drawman/task_dm5.py
+++ drawman/task_dm5.py
@@ -5,28 +5,36 @@ from time import sleep
 def list1():
     """Рисуем лист, справой стороны стебля"""
     t.pendown()
+    t.width(3)
+    t.begin_fill()
     for i in range(2):
         for i in range(90):
             t.forward(1.5)
             t.right(1)
         t.right(90)
+    t.end_fill()
     t.penup()
 
 
 def list2():
     """Рисуем лист, слевой стороны стебля"""
     t.pendown()
+    t.width(3)
+    t.begin_fill()
     for i in range(2):
         for i in range(90):
             t.forward(1.5)
             t.left(1)
         t.left(90)
+    t.end_fill()
     t.penup()
 
 
 def list3():
     """Рисуем лепесток цветка"""
     t.pendown()
+    t.width(3)
+    t.begin_fill()
     for i in range(90):
         t.forward(1.5)
         t.right(1)
@@ -37,20 +45,26 @@ def list3():
     for i in range(90):
         t.forward(3)
         t.right(1)
+    t.end_fill()
     t.right(90)
     t.penup()
 
 
+
+t.color('green', '#00DD00')
 t.left(90)
 t.penup()
 t.backward(350)
 t.pendown()
+t.width(5)
 t.forward(70)
 list1()
 t.pendown()
+t.width(5)
 t.forward(80)
 list2()
 t.pendown()
+t.width(5)
 t.forward(340)
 for i in range(10):
     list3()
